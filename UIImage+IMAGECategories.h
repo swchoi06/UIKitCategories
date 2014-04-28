e2df6d0eda700941c7bfa61a281681d80961a223
swchoi06 <swchoi06@wafflestudio.com>
2014-04-28 08:41:14 +0200

    Initial Commit

---
+++ UIImage+IMAGECategories.h
@@ -0,0 +1,14 @@
+//
+//  UIImage+IMAGECategories.h
+//  UIKitCategories
+//
+//  Created by SukWon Choi on 13. 8. 27..
+//  Copyright (c) 2013년 Wafflestudio. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UIImage(IMAGECategories)
++ (UIImage *)image1x1WithColor:(UIColor *)color;
+
+@end
---
+++ UIImage+IMAGECategories.m
@@ -0,0 +1,23 @@
+//
+//  UIImage+IMAGECategories.m
+//  UIKitCategories
+//
+//  Created by SukWon Choi on 13. 8. 27..
+//  Copyright (c) 2013년 Wafflestudio. All rights reserved.
+//
+
+#import "UIImage+IMAGECategories.h"
+
+@implementation UIImage(IMAGECategories)
++ (UIImage *)image1x1WithColor:(UIColor *)color
+{
+    CGRect rect = CGRectMake(0.0f, 0.0f, 1.0f, 1.0f);
+    UIGraphicsBeginImageContext(rect.size);
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGContextSetFillColorWithColor(context, [color CGColor]);
+    CGContextFillRect(context, rect);
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return image;
+}
+@end
